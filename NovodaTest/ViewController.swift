b8af9a38e34246a3fd3398f9fbd55f8f33be34a8
omerjanjuabbc <omer.janjua.ext@bbc.co.uk>
2026-04-26 13:28:06 +0100

    Hooked up standard tableView

--- NovodaTest/ViewController.swift
+++ NovodaTest/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableView.self))
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: String(describing: UITableView.self)) else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+    }
 }
 
